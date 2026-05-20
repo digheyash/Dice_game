84be27f2a9521a76eaacd2c015b7d92b5f344c2d
digheyash <digheyashoff@gmail.com>
2026-05-20 08:59:28 +0530

    commit

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var randomImageSource2 = "./images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}   
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
